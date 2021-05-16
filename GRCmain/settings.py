42e08eb2ca38f30e36c61857dcc697d095306004
mahan-h23 <mahan.hassani@digikala.com>
2021-05-16 15:28:01 +0430

    add indicators

--- GRCmain/settings.py
+++ GRCmain/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'risks_mitigations',
+    'indicators'
 
 ]
 
---
+++ indicators/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+
+def viewList(request):
+    return render(request,'')
